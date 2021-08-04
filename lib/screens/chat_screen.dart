3ac8ea0b69de3354888105614e76525361e3351e
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-08-04 18:05:05 +0900

    8. 채팅 화면 완성하기

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao/components/chat_card.dart';
+import 'package:flutter_kakao/models/chat.dart';
 
 class ChatScreen extends StatelessWidget {
   // const ChatScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("ChatScreen"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(title: Text("채팅")),
+      body: ListView(
+        children: List.generate(
+            chats.length,
+            (index) => ChatCard(chat: chats[index]),
+        ),
+      ),
     );
   }
 }
