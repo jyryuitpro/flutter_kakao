842a6ebc4cf12400e3a38c3a2ea1c43144020f9b
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-08-04 21:51:41 +0900

    12. 더보기 화면 만들기

--- lib/screens/more_screen.dart
+++ lib/screens/more_screen.dart
@@ -1,12 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao/models/tab.dart';
 
 class MoreScreen extends StatelessWidget {
   // const MoreScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("MoreScreen"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("더보기"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 30),
+        child: GridView.count(
+          crossAxisCount: 4,
+          children: List.generate(
+            tabs.length,
+                (index) => Column(
+              children: [
+                Icon(tabs[index].icon),
+                SizedBox(height: 5),
+                Text(tabs[index].text),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
