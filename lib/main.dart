37135cd714d2a0ba19db05c0caaccceb08392637
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-08-04 11:40:41 +0900

    2. IndexedStack과 BottomNavigationBar로 MainScreen 완성하기

--- lib/main.dart
+++ lib/main.dart
@@ -6,20 +6,21 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  // const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-          appBarTheme: AppBarTheme(
-            elevation: 0.0,
-            backgroundColor: Colors.white,
-            textTheme: TextTheme(
-              headline6: TextStyle(color: Colors.black, fontSize: 24),
-            ),
-            iconTheme: IconThemeData(color: Colors.black),
+        appBarTheme: AppBarTheme(
+          elevation: 0.0,
+          backgroundColor: Colors.white,
+          textTheme: TextTheme(
+            headline6: TextStyle(color: Colors.black, fontSize: 24),
           ),
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
       ),
       home: MainScreen(),
     );
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,18 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao/screens/chat_screen.dart';
+import 'package:flutter_kakao/screens/friend_screen.dart';
+import 'package:flutter_kakao/screens/friend_screen.dart';
+import 'package:flutter_kakao/screens/friend_screen.dart';
+import 'package:flutter_kakao/screens/more_screen.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class MainScreen extends StatefulWidget {
+  // const MainScreen({Key? key}) : super(key: key);
 
   @override
   _MainScreenState createState() => _MainScreenState();
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("MainScreen"),
-      )
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          FriendScreen(),
+          ChatScreen(),
+          MoreScreen(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _selectedIndex,
+        backgroundColor: Colors.grey[100],
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black54,
+        onTap: (index) {
+          print("선택된 번호 : $index");
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.user),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.comment),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.ellipsisH),
+            label: "",
+          ),
+        ],
+      ),
     );
   }
 }
